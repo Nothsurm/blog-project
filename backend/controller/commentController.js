86a608f964c27e29b328127c648d5edb1ccc9009
Mike Rushton <mrush94ton@gmail.com>
2024-02-04 17:03:56 +0000

    added like functionality and route to comment component

--- backend/controller/commentController.js
+++ backend/controller/commentController.js
@@ -1,3 +1,4 @@
+import { errorHandler } from "../middleware/error.js";
 import Comment from "../models/commentModel.js";
 
 export const createComment = async (req, res, next) => {
@@ -24,6 +25,28 @@ export const getPostComments = async (req, res, next) => {
     try {
         const comments = await Comment.find({ postId: req.params.postId }).sort({createdAt: -1})
         res.status(200).json(comments)
+    } catch (error) {
+        next(error)
+    }
+};
+
+export const likeComment = async (req, res, next) => {
+    try {
+        const comment = await Comment.findById(req.params.commentId)
+        if (!comment) {
+            return next(errorHandler(404, 'Comment not found'))
+        }
+        const userIndex = comment.likes.indexOf(req.user.id)
+        //indexOf returns -1 if the element is not present
+        if (userIndex === -1) {
+            comment.numberOfLikes += 1;
+            comment.likes.push(req.user.id)
+        } else {
+            comment.numberOfLikes -= 1;
+            comment.likes.splice(userIndex, 1)
+        }
+        await comment.save()
+        res.status(200).json(comment)
     } catch (error) {
         next(error)
     }
