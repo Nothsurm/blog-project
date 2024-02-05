eafc959cee595c2fa0d9cd6feebf90196bd334ae
Mike Rushton <mrush94ton@gmail.com>
2024-02-05 20:39:36 +0000

    complete about and projects ui pages

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -2,6 +2,16 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>About</div>
+    <div className='min-h-screen flex items-center justify-center'>
+      <div className="max-w-2xl mx-auto p-3 text-center">
+        <div>
+          <h1 className='text-3xl font-semibold text-center my-7'>About Rushton's Blog</h1>
+          <div className="text-md text-gray-500 flex flex-col gap-6">
+            <p>I have extensive experience in programming and software development, with a particular focus on JavaScript. With my deep understanding of programming principles and best practices, I am confident in my ability to deliver high-quality and efficient code solutions.</p>
+            <p>I keep up-to-date with the latest developments in the industry, ensuring that my work is always at the forefront of modern programming techniques. Whether you need assistance with front-end development, back-end systems, or full-stack solutions, I am here to help you achieve your goals. Let's work together to create innovative and robust software solutions that meet your specific needs.</p>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
